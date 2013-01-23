804b96d5bd51b8941b8fe30269531f25840d185a
codeworkx <codeworkx@cyanogenmod.org>
2013-01-23 15:56:15 +0000

    SamsungExynos3RIL: fix unable to swtich 2G/3G while connected to wifi

--- src/java/com/android/internal/telephony/SamsungExynos4RIL.java
+++ src/java/com/android/internal/telephony/SamsungExynos4RIL.java
@@ -168,11 +168,60 @@ public class SamsungExynos4RIL extends RIL implements CommandsInterface {
     dialEmergencyCall(String address, int clirMode, Message result) {
         RILRequest rr;
         Rlog.v(RILJ_LOG_TAG, "Emergency dial: " + address);
-
         rr = RILRequest.obtain(RIL_REQUEST_DIAL_EMERGENCY, result);
         rr.mParcel.writeString(address + "/");
         rr.mParcel.writeInt(clirMode);
         rr.mParcel.writeInt(0);  // UUS information is absent
+        // Get raw data
+        response = new int[numInts];
+        for (int i = 0 ; i < numInts ; i++) {
+            response[i] = p.readInt();
+        }
+
+        /*
+        Log.d(LOG_TAG, "gsmSignalStrength=" + response[0]);
+        Log.d(LOG_TAG, "gsmBitErrorRate=" + response[1]);
+        Log.d(LOG_TAG, "cdmaDbm=" + response[2]);
+        Log.d(LOG_TAG, "cdmaEcio=" + response[3]);
+        Log.d(LOG_TAG, "evdoDbm=" + response[4]);
+        Log.d(LOG_TAG, "evdoEcio=" + response[5]);
+        Log.d(LOG_TAG, "evdoSnr=" + response[6]);
+        Log.d(LOG_TAG, "lteSignalStrength=" + response[7]);
+        Log.d(LOG_TAG, "lteRsrp=" + response[8]);
+        Log.d(LOG_TAG, "lteRsrq=" + response[9]);
+        Log.d(LOG_TAG, "lteRssnr=" + response[10]);
+        Log.d(LOG_TAG, "lteCqi=" + response[11]);
+        */
+
+        int mGsmSignalStrength = response[0]; // Valid values are (0-31, 99) as defined in TS 27.007 8.5
+        Log.d(LOG_TAG, "responseSignalStrength (raw): gsmSignalStrength=" + mGsmSignalStrength);
+        mGsmSignalStrength = mGsmSignalStrength & 0xff; // Get the first 8 bits
+        Log.d(LOG_TAG, "responseSignalStrength (corrected): gsmSignalStrength=" + mGsmSignalStrength);
+
+        /* if mGsmSignalStrength isn't a valid value, use mCdmaDbm as fallback */
+        if (mGsmSignalStrength < 0 || (mGsmSignalStrength > 31 && response[0] != 99)) {
+            int mCdmaDbm = response[2];
+            Log.d(LOG_TAG, "responseSignalStrength-fallback (raw): gsmSignalStrength=" + mCdmaDbm);
+
+	        if (mCdmaDbm < 0) {
+	            mGsmSignalStrength = 99;
+	        } else if (mCdmaDbm > 31 && mCdmaDbm != 99) {
+	            mGsmSignalStrength = 31;
+	        } else {
+	            mGsmSignalStrength = mCdmaDbm;
+	        }
+            Log.d(LOG_TAG, "responseSignalStrength-fallback (corrected): gsmSignalStrength=" + mGsmSignalStrength);
+        }
+
+        SignalStrength signalStrength = new SignalStrength(mGsmSignalStrength, response[1], response[2],
+                    response[3], response[4], response[5], response[6], isGsm);
+
+        return signalStrength;
+    }
+
+    @Override public void
+    getVoiceRadioTechnology(Message result) {
+        RILRequest rr = RILRequest.obtain(RIL_REQUEST_VOICE_RADIO_TECH, result); signalstrength is invalid
 
         if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
 
