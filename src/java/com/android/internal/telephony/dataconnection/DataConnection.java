a20d2bc782be6bb40f1c5aa65da79ece1c5ed566
Muhammed Siju <msiju@codeaurora.org>
2015-04-24 19:36:07 +0530

    Handle out of sequence disconnect event in DcTracker.
    Ignore disconnect event if ApnContext is in CONNECTING state.
    When entering DcRetrying state, if phone is in OOS, first notify
    Dc retrying event and then disconnected event.
    This ensures ApnContext will always move out of CONNECTING
    state before disconnecting.
    
    Change-Id: I1c83326be18bec7f41327d9a8d4c1839c0016354
    CRs-Fixed: 828349

--- src/java/com/android/internal/telephony/dataconnection/DataConnection.java
+++ src/java/com/android/internal/telephony/dataconnection/DataConnection.java
@@ -1411,6 +1411,7 @@ public final class DataConnection extends StateMachine {
                         + " transitionTo(mInactiveState)";
                     logAndAddLogRec(s);
                 }
+                notifyAllOfDisconnectDcRetrying(Phone.REASON_LOST_DATA_CONNECTION);
                 mInactiveState.setEnterNotificationParams(DcFailCause.LOST_CONNECTION);
                 transitionTo(mInactiveState);
             } else {
