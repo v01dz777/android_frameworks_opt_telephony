f44a3b9034d2e129cc5c5e7f003aaed8d53edef9
Umashankar Godachi <umasha@codeaurora.org>
2015-07-01 17:42:05 +0530

    MSIM: Fix phone app crash during radio unavailability.
    In scenarios where radio is unavailabile due to modem
    SSR, and a request arrives for setting the preferred network
    type, there is a phone app crash seen in ModemBindingPolicyHandler
    due to null value of modem capability info.
    
    Fix: Handle Radio unavailable event in ModemBindingPolicyHandler
    by setting the Modem Capabilities to false. Also check for null
    value, while accessing  modem capability info.
    
    Change-Id: I41d3221adb1d6a67b1b5d9729e128ac8e6147039
    CRs-Fixed: 863724.

--- src/java/com/android/internal/telephony/ModemBindingPolicyHandler.java
+++ src/java/com/android/internal/telephony/ModemBindingPolicyHandler.java
@@ -154,6 +154,7 @@ public class ModemBindingPolicyHandler extends Handler {
     private static final int EVENT_MODEM_RAT_CAPS_AVAILABLE = 1;
     private static final int EVENT_UPDATE_BINDING_DONE = 2;
     private static final int EVENT_SET_NW_MODE_DONE = 3;
+    private static final int EVENT_RADIO_NOT_AVAILABLE = 4;
 
     //*****Constants
     private static final int SUCCESS = 1;
@@ -210,6 +211,10 @@ public class ModemBindingPolicyHandler extends Handler {
         mModemStackController.registerForModemRatCapsAvailable
                 (this, EVENT_MODEM_RAT_CAPS_AVAILABLE, null);
 
+        for (int i = 0; i < mCi.length; i++) {
+            mCi[i].registerForNotAvailable(this, EVENT_RADIO_NOT_AVAILABLE, null);
+        }
+
         for (int i = 0; i < mNumPhones; i++) {
             mPreferredStackId[i] = i;
             mCurrentStackId[i] = i;
@@ -240,6 +245,10 @@ public class ModemBindingPolicyHandler extends Handler {
                 handleSetPreferredNetwork(msg);
                 break;
 
+            case EVENT_RADIO_NOT_AVAILABLE:
+                logd("EVENT_RADIO_NOT_AVAILABLE");
+                handleModemRatCapsUnAvailable();
+                break;
             default:
                 break;
         }
@@ -330,6 +339,12 @@ public class ModemBindingPolicyHandler extends Handler {
         mModemRatCapabilitiesAvailable = true;
         //Initialization sequence: Need to send Bind request always, so override is true.
         if (SUCCESS == updateStackBindingIfRequired(true)) mIsSetPrefNwModeInProgress = true;
+    }
+
+    private void handleModemRatCapsUnAvailable() {
+        if (mModemRatCapabilitiesAvailable) {
+            mModemRatCapabilitiesAvailable = false;
+        }
     }
 
     private void syncCurrentStackInfo() {
@@ -510,6 +525,11 @@ public class ModemBindingPolicyHandler extends Handler {
             return supportedRatMaskForNwMode;
         }
         logd("getNumOfRATsSupportedForNwMode: nwMode[" + nwMode +"] modemCaps = " + modemCaps);
+
+        if (modemCaps == null) {
+            loge("getNumOfRATsSupportedForNwMode: Modem Capabilites are null. Return!!");
+            return 0;
+        }
 
         //send result by ANDing corresponding NETWORK MASK and Modem Caps mask.
         switch (nwMode) {
